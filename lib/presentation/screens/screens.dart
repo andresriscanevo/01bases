6d43226773e3e6109c0b8b6b5d8cebd2008fcb02
andresriscanevo <andresriscanevo10@gmail.com>
2023-08-22 21:12:39 -0500

    Screens creadas

--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -1,3 +1,9 @@
+export 'package:widgetsapp/presentation/screens/animated/animated_screen.dart';
+export 'package:widgetsapp/presentation/screens/app_tutorial/app_tutorial_screen.dart';
+export 'package:widgetsapp/presentation/screens/infinite_scroll/infinite_scroll_screen.dart';
+export 'package:widgetsapp/presentation/screens/progress/progress_screen.dart';
+export 'package:widgetsapp/presentation/screens/snackbar/snackbar_screen.dart';
+export 'package:widgetsapp/presentation/screens/ui_constrols/ui_controls_screnn.dart';
 export 'package:widgetsapp/presentation/screens/cards/cards_screen.dart';
 export 'package:widgetsapp/presentation/screens/home/home_screen.dart';
 export 'package:widgetsapp/presentation/screens/buttons/buttons_screen.dart';
